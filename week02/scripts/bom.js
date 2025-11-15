b3fc98f67451f2e094de177b25cad9b39f7557a3
jonas chino <chinojonas99@gmail.com>
2025-11-15 10:45:15 -0400

    week2

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,3 +1,24 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('')
+button.addEventListener('click', function () {
+    // Code to execute when the button is clicked
+    if (input.value =  '') {
+        const input = document.querySelector('#favchap');
+        const button = document.querySelector('button');
+        const list = document.querySelector('4');
+        const li = document.createElement('li');
+
+        const deleteButon = document.createElement('button');
+
+        li.textContent = input.value;
+
+        deleteButon.textContent = 'x';
+
+        li.append(deleteButon);
+
+        list.append(li)
+    }
+});
+
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
