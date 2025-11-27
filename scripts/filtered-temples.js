11f54ae92a7982c72a09637a04739be4a47667c5
jonas chino <chinojonas99@gmail.com>
2025-11-26 23:49:11 -0400

    w04mid

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,3 +1,25 @@
+const currentYear = new Date().getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const menuToggle = document.getElementById('menu_toggle');
+const navMenu = document.querySelector('.nav_menu');
+
+if (menuToggle && navMenu) {
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+        
+        // Toggle between hamburger and X symbol
+        if (navMenu.classList.contains('show')) {
+            menuToggle.textContent = '✕';
+        } else {
+            menuToggle.textContent = '☰';
+        }
+    });
+}
+
+
+
+
 const temples = [
   {
     templeName: "Aba Nigeria",
