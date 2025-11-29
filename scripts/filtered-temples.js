8a8b0f236f3a13d69c76b7371e3caa14ec45b785
jonas chino <chinojonas99@gmail.com>
2025-11-29 19:41:44 -0400

    w04

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -120,7 +120,6 @@ function displayTemples(templeList) {
     templesContainer.innerHTML = templeList.map(createTempleCard).join('');
 }
 
-
 document.addEventListener('DOMContentLoaded', () => {
     displayTemples(temples);
 
